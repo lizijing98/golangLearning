be97735b1e43ee814233a15bdfdc493d0636a534
lizijing98 <lizijing98@live.com>
2021-10-10 19:36:14 +0800

    feat: 增加 JSON

---
+++ src/20_Internet/http/20.5_json.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	Id     int
+	Name   string
+	Age    int
+	gender string //gender 是小写,小写字母开头在 JSON 编码时会忽略掉
+}
+
+func main() {
+	xiaoming := Student{
+		Id:     1,
+		Name:   "xiaoming",
+		Age:    20,
+		gender: "man",
+	}
+
+	//1.编码（序列化）
+	//func Marshal(v interface{}) ([]byte, error)
+	encodeInfo, err := json.Marshal(&xiaoming) //函数参数是接口，传地址
+	if err != nil {
+		fmt.Println("json.Marshal err:", err)
+		return
+	}
+	fmt.Println("encodeInfo:", string(encodeInfo))
+
+	//2.解码
+	var xiaoming2 Student
+	err = json.Unmarshal([]byte(encodeInfo), &xiaoming2)
+	if err != nil {
+		fmt.Println("json.Unmarshal err:", err)
+		return
+	}
+	fmt.Println("xiaoming name:", xiaoming2.Name)
+	fmt.Println("xiaoming gender:", xiaoming2.gender)
+}
